bdd9fc3723ef57d1207c6947e5a4d144dcf10ef1
Patrick T Coakley <contact@patricktcoakley.com>
2023-09-05 12:40:41 -0400

    Paralellize sampling; refactor shared variables to fields

--- RTIOW/Program.cs
+++ RTIOW/Program.cs
@@ -8,75 +8,92 @@ namespace RTIOW;
 
 internal static class Program
 {
+    private const int ImageWidth = 1200;
+    private const float AspectRatio = 3.0f / 2.0f;
+    private const int ImageHeight = (int)(ImageWidth / AspectRatio);
+    private const int SamplesPerPixel = 500;
+    private const int MaxDepth = 50;
+    private const float FocusDistance = 10.0f;
+    private const float Aperture = 0.1f;
+    private static readonly HittableList World = RandomScene();
+
     public static void Main(string[] args)
     {
-        const float aspectRatio = 3.0f / 2.0f;
-        const int imageWidth = 400;
-        const int imageHeight = (int)(imageWidth / aspectRatio);
-        const int samplesPerPixel = 100;
-        const int maxDepth = 50;
-
-        var world = RandomScene();
-
         var lookFrom = new Vector3(13.0f, 2.0f, 3.0f);
         var lookAt = new Vector3(0.0f, 0.0f, 0.0f);
         var verticalUp = new Vector3(0.0f, 1.0f, 0.0f);
-        const float focusDistance = 10.0f;
-        const float aperture = 0.1f;
         var camera = new Camera(
             lookFrom,
             lookAt,
             verticalUp,
             20.0f,
-            aspectRatio,
-            aperture,
-            focusDistance);
+            AspectRatio,
+            Aperture,
+            FocusDistance);
 
-        using var ppmFile = new PpmCanvas(imageWidth, imageHeight);
+        using var ppmFile = new PpmCanvas(ImageWidth, ImageHeight);
 
-        for (var y = imageHeight - 1; y >= 0; --y)
+        var pixels = new Vector3[ImageWidth, ImageHeight];
+        Parallel.For(0, ImageWidth, x =>
         {
-            for (var x = 0; x < imageWidth; ++x)
+            for (var y = ImageHeight - 1; y >= 0; --y)
             {
-                var pixel = new Vector3();
-                var hr = new HitRecord();
-                for (var s = samplesPerPixel; s > 0; s--)
-                {
-                    var u = (x + Random.Shared.NextSingle()) / (imageWidth - 1);
-                    var v = (y + Random.Shared.NextSingle()) / (imageHeight - 1);
-                    var ray = camera.Ray(u, v);
-                    pixel += HitColor(ray, world, maxDepth, ref hr);
-                }
+                var pixel = SamplePixel(x, y, camera);
+                pixels[x, y] = pixel;
+            }
+        });
 
-                ppmFile.WriteColor(pixel.ToColor(samplesPerPixel));
+        for (var y = ImageHeight - 1; y >= 0; --y)
+        {
+            for (var x = 0; x < ImageWidth; ++x)
+            {
+                ppmFile.WriteColor(pixels[x, y].ToColor(SamplesPerPixel));
             }
         }
+    }
 
-        return;
 
-        Vector3 HitColor(Ray ray, IHittable hittable, int depth, ref HitRecord hr)
+    private static Vector3 SamplePixel(
+        int x,
+        int y,
+        Camera camera
+    )
+    {
+        var pixel = new Vector3();
+        var hr = new HitRecord();
+        for (var s = SamplesPerPixel; s > 0; s--)
         {
-            if (depth <= 0)
-            {
-                return Vector3.Zero;
-            }
-
-            if (hittable.Hit(ray, 0.001f, float.PositiveInfinity, ref hr))
-            {
-                var scattered = new Ray();
-                var attenuation = Vector3.Zero;
-                if (hr.Material!.Scatter(ray, hr, ref attenuation, ref scattered))
-                {
-                    return attenuation * HitColor(scattered, world, depth - 1, ref hr);
-                }
-
-                return Vector3.Zero;
-            }
-
-            var unitDirection = Vector3.Normalize(ray.Direction);
-            var t = 0.5f * (unitDirection.Y + 1.0f);
-            return (1.0f - t) * Vector3.One + t * new Vector3(0.5f, 0.7f, 1.0f);
+            var u = (x + Random.Shared.NextSingle()) / (ImageWidth - 1);
+            var v = (y + Random.Shared.NextSingle()) / (ImageHeight - 1);
+            var ray = camera.Ray(u, v);
+            pixel += HitColor(ray, World, MaxDepth, ref hr);
         }
+
+        return pixel;
+    }
+
+    private static Vector3 HitColor(Ray ray, IHittable hittable, int depth, ref HitRecord hr)
+    {
+        if (depth <= 0)
+        {
+            return Vector3.Zero;
+        }
+
+        if (hittable.Hit(ray, 0.001f, float.PositiveInfinity, ref hr))
+        {
+            var scattered = new Ray();
+            var attenuation = Vector3.Zero;
+            if (hr.Material!.Scatter(ray, hr, ref attenuation, ref scattered))
+            {
+                return attenuation * HitColor(scattered, World, depth - 1, ref hr);
+            }
+
+            return Vector3.Zero;
+        }
+
+        var unitDirection = Vector3.Normalize(ray.Direction);
+        var t = 0.5f * (unitDirection.Y + 1.0f);
+        return (1.0f - t) * Vector3.One + t * new Vector3(0.5f, 0.7f, 1.0f);
     }
 
     private static HittableList RandomScene()
@@ -84,7 +101,6 @@ internal static class Program
         var world = new HittableList();
         var ground = new Lambertian(new Vector3(0.5f, 0.5f, 0.5f));
         world.Add(new Sphere(new Vector3(0.0f, -1000.0f, 0.0f), 1000.0f, ground));
-
 
         for (var a = -11; a < 11; a++)
         {
@@ -119,15 +135,14 @@ internal static class Program
             }
         }
 
-        var material1 = new Dielectric(1.5f);
-        world.Add(new Sphere(new Vector3(0.0f, 1.0f, 0.0f), 1.0f, material1));
+        var dielectric = new Dielectric(1.5f);
+        world.Add(new Sphere(new Vector3(0.0f, 1.0f, 0.0f), 1.0f, dielectric));
 
-        var material2 = new Lambertian(new Vector3(0.4f, 0.2f, 0.1f));
-        world.Add(new Sphere(new Vector3(-4.0f, 1.0f, 0.0f), 1.0f, material2));
+        var lambertian = new Lambertian(new Vector3(0.4f, 0.2f, 0.1f));
+        world.Add(new Sphere(new Vector3(-4.0f, 1.0f, 0.0f), 1.0f, lambertian));
 
-        var material3 = new Metal(new Vector3(0.7f, 0.6f, 0.5f), 0.0f);
-        world.Add(new Sphere(new Vector3(4.0f, 1.0f, 0.0f), 1.0f, material3));
-
+        var metal = new Metal(new Vector3(0.7f, 0.6f, 0.5f), 0.0f);
+        world.Add(new Sphere(new Vector3(4.0f, 1.0f, 0.0f), 1.0f, metal));
 
         return world;
     }
